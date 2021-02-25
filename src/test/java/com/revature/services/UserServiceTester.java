e51397ab910c1728c660837622f6e89b5ff8477c
Brian-Revature <brian.withrow@revature.net>
2021-02-25 11:59:17 -0500

    Adding tests to github for viewing purposes-- all running with Mockito mocks working properly.

--- src/test/java/com/revature/services/UserServiceTester.java
+++ src/test/java/com/revature/services/UserServiceTester.java
@@ -1,17 +1,25 @@
 package com.revature.services;
+import com.revature.exceptions.InvalidCredentialsException;
+import com.revature.exceptions.PersistenceException;
 import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTester {
 
     User testUser = new User("TestUser", "TestPass", "Tester",
@@ -29,7 +37,6 @@ public class UserServiceTester {
 
     @Before
     public void testSetUp() {
-        userService = new UserService();
         users = new ArrayList<>();
         users.add(testUser);
         users.add(fullTest);
@@ -49,7 +56,91 @@ public class UserServiceTester {
 
         //Assert
         Assert.assertEquals(myUsers, users);
-
     }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_Authentication_null_user(){
+        //Arrange
+        String username = null;
+        String password = "pass";
+
+        //Act
+        userService.authenticate(username, password);
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_Authentication_trim_user(){
+        //Arrange
+        String username = "";
+        String password = "pass";
+
+        //Act
+        userService.authenticate(username, password);
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_Authentication_null_password(){
+        //Arrange
+        String username = "user";
+        String password = null;
+
+        //Act
+        userService.authenticate(username, password);
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_Authentication_trim_pass(){
+        //Arrange
+        String username = "user";
+        String password = "";
+
+        //Act
+        userService.authenticate(username, password);
+    }
+
+    @Test
+    public void test_authenticate_valid(){
+        //Arrange
+        when(userRepository.getAUserByUsernameAndPassword("TestUser","TestPass"))
+                .thenReturn(Optional.of(testUser));
+
+        //Act
+        User user = userService.authenticate("TestUser", "TestPass");
+
+        //Assert
+        Assert.assertEquals(user, testUser);
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void test_authenticate_invalid(){
+        //Arrange
+        when(userRepository.getAUserByUsernameAndPassword("TestUser","TestPass"))
+                .thenReturn(Optional.empty());
+
+        //Act
+        User user = userService.authenticate("TestUser", "TestPass");
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_register_invalidUser(){
+        //Arrange
+        User user = null;
+
+        //Act
+        userService.register(user);
+    }
+
+    @Test(expected = InvalidCredentialsException.class)
+    public void test_register_existingUser_notPresent(){
+        //Arrange
+        User user = null;
+        when(userRepository.getAUserByUsername("TestUser"))
+                .thenReturn(Optional.of(testUser));
+
+        //Act
+        userService.register(testUser);
+    }
+
+
 
 }
