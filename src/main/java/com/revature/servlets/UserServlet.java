a2731e25b80207ccf5fb580446f7bc40f492a5f0
Nate Gamble <nate.eli.gamble@gmail.com>
2021-02-26 10:06:28 -0500

    changed user servlet annotation

--- src/main/java/com/revature/servlets/UserServlet.java
+++ src/main/java/com/revature/servlets/UserServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "users", urlPatterns = "/users")
+@WebServlet("/users")
 public class UserServlet extends HttpServlet {
 
     @Override
